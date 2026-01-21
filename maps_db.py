ec7b5c4273678075ee56d41a6fb8ebbcecd7ce97
pch38861970 <pc.hung1555@gmail.com>
2026-01-21 15:47:03 +0800

    Create maps_db.py

---
+++ maps_db.py
@@ -0,0 +1,8 @@
+# 關渡之戰背景：以華北為核心
+cities = {
+    1: {"name": "許昌", "region": "豫州", "connections": [2, 3]},
+    2: {"name": "官渡", "region": "兗州", "connections": [1, 4]},
+    3: {"name": "洛陽", "region": "司隸", "connections": [1]},
+    4: {"name": "鄴城", "region": "冀州", "connections": [2]},
+    # ... 需定義 30+ 城市
+}
